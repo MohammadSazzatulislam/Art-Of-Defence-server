e749e841174fc6e91a8366173b3790c290a9c913
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-08 18:33:16 +0600

    add mongodb

--- index.js
+++ index.js
@@ -4,14 +4,40 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors())
 app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send('api comming soon')
-})
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4lwt8qz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db("artDefensee").collection("servicess");
+        
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const userServices = serviceCollection.find(query)
+            const services = await userServices.limit(3).toArray()
+            res.send(services)
+        })
+
+
+
+    } finally {
+        
+    }
+}
+run().catch(err=> console.log(err))
 
 
 
