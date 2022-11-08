72252bb7c16378473f8aaf59d9319c22f5814d0e
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-08 22:38:10 +0600

    add service api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors())
 app.use(express.json())
@@ -31,6 +31,20 @@ async function run() {
             res.send(services)
         })
 
+        app.get('/service', async(req, res) => {
+            const query = {}
+            const userServices = serviceCollection.find(query)
+            const services = await userServices.toArray()
+            res.send(services)
+        })
+
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id : ObjectId(id)}
+            const userServices = await serviceCollection.findOne(query)
+            res.send(userServices);
+        })
+        
 
 
     } finally {
